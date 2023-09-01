4c43be2f792a21cd67dd333260fc18486cac5cd9
tfengaka <nthoa2.lhp.nh@gmail.com>
2023-09-01 16:16:27 +0700

    backup

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import CssBaseline from '@mui/material/CssBaseline';
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Routers from '~/routes';
 import theme from './theme';
 
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <CssVarsProvider theme={theme}>
-      <CssBaseline />
-      <Routers />
-    </CssVarsProvider>
-  </React.StrictMode>
+  <CssVarsProvider theme={theme}>
+    <CssBaseline />
+    <Routers />
+  </CssVarsProvider>
 );
--- src/pages/Workspace/components/Boards/BoardContent.tsx
+++ src/pages/Workspace/components/Boards/BoardContent.tsx
@@ -43,20 +43,19 @@ function BoardContent({ data: { listData, listOrderIds } }: Props) {
   const sensors = useSensors(
     useSensor(MouseSensor, {
       activationConstraint: {
-        distance: 25,
+        distance: 15,
       },
     }),
     useSensor(TouchSensor, {
       activationConstraint: {
-        delay: 250,
+        delay: 150,
         tolerance: 250,
       },
     })
   );
 
   const [listOrdered, setListOrdered] = useState<Array<List>>([]);
-
-  const [activeId, setActiveId] = useState<UniqueIdentifier | null>(null);
+  const [activeListId, setActiveListId] = useState<UniqueIdentifier | null>(null);
   const [activeItem, setActiveItem] = useState<any>(null);
 
   useEffect(() => {
@@ -65,13 +64,50 @@ function BoardContent({ data: { listData, listOrderIds } }: Props) {
 
   const findListByCardId = (cardId: string) => listOrdered.find((e) => e.cards.map((c) => c._id)?.includes(cardId));
 
+  const handleDragCardToDiffList = (activeList: List, overList: List, { active, over }: DragEndEvent | DragOverEvent) =>
+    setListOrdered((prev) => {
+      if (!over) return prev;
+
+      const overCardIndex = overList.cards.findIndex((c) => c._id === over.id);
+
+      const isBelowOverItem =
+        active.rect.current.translated && active.rect.current.translated.top > over.rect.top + over.rect.height;
+
+      const modifier = isBelowOverItem ? 1 : 0;
+
+      const newIndex = overCardIndex >= 0 ? overCardIndex + modifier : overList.cards.length + 1;
+
+      const cloneLists = cloneDeep(prev);
+      const newActiveList = cloneLists.find((c) => c._id === activeList._id);
+      const newOverList = cloneLists.find((c) => c._id === overList._id);
+
+      if (newActiveList) {
+        newActiveList.cards = newActiveList.cards.filter((c) => c._id !== active.id);
+        newActiveList.cardOrderIds = newActiveList.cards.map((c) => c._id);
+      }
+
+      if (newOverList) {
+        newOverList.cards = newOverList.cards.filter((c) => c._id !== active.id);
+        newOverList.cards = [
+          ...newOverList.cards.slice(0, newIndex),
+          active.data.current as Card,
+          ...newOverList.cards.slice(newIndex),
+        ];
+        newOverList.cardOrderIds = newOverList.cards.map((c) => c._id);
+      }
+
+      return cloneLists;
+    });
+
   const handleDragStart = ({ active }: DragStartEvent) => {
-    setActiveId(active.id);
+    setActiveListId(active.data.current?.listId ? active.data.current.listId : active.data.current?._id);
     setActiveItem(active?.data.current);
   };
 
-  const handleDragOver = ({ active, over }: DragOverEvent) => {
-    if (!over || !activeId || !activeItem.listId) return;
+  const handleDragOver = (event: DragOverEvent) => {
+    const { active, over } = event;
+
+    if (!over || !activeItem.listId) return;
 
     const activeList = findListByCardId(active.id as string);
     const overList = findListByCardId(over.id as string);
@@ -79,63 +115,60 @@ function BoardContent({ data: { listData, listOrderIds } }: Props) {
     if (!activeList || !overList) return;
 
     if (activeList._id !== overList._id) {
-      setListOrdered((prev) => {
-        const overCardIndex = overList.cards.findIndex((c) => c._id === over.id);
-
-        const isBelowOverItem =
-          active.rect.current.translated && active.rect.current.translated.top > over.rect.top + over.rect.height;
-
-        const modifier = isBelowOverItem ? 1 : 0;
-
-        const newIndex = overCardIndex >= 0 ? overCardIndex + modifier : overList.cards.length + 1;
-
-        const cloneLists = cloneDeep(prev);
-        const newActiveList = cloneLists.find((c) => c._id === activeList._id);
-        const newOverList = cloneLists.find((c) => c._id === overList._id);
-
-        if (newActiveList) {
-          newActiveList.cards = newActiveList.cards.filter((c) => c._id !== active.id);
-          newActiveList.cardOrderIds = newActiveList.cards.map((c) => c._id);
-        }
-
-        if (newOverList) {
-          newOverList.cards = newOverList.cards.filter((c) => c._id !== active.id);
-          newOverList.cards = newOverList.cards.toSpliced(newIndex, 0, active.data.current as Card);
-          newOverList.cardOrderIds = newOverList.cards.map((c) => c._id);
-        }
-
-        return cloneLists;
-      });
+      handleDragCardToDiffList(activeList, overList, event);
     }
   };
 
-  const handleDragEnd = ({ active, over }: DragEndEvent) => {
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
     if (!over) {
-      setActiveId(null);
+      setActiveListId(null);
       setActiveItem(null);
       return;
     }
 
-    if (active.data.current?.listId) {
-      console.log('Drag Card is active');
-      return;
+    if (activeItem?.listId) {
+      const activeList = listData.find((item) => item._id === activeListId);
+      const overList = findListByCardId(over.id as string);
+
+      if (!activeList || !overList) return;
+
+      if (activeListId !== overList._id) {
+        console.log('Drag card to other list');
+        handleDragCardToDiffList(activeList, overList, event);
+      } else {
+        const oldCardIndex = activeList.cardOrderIds.indexOf(activeItem._id);
+        const newCardIndex = overList.cardOrderIds.indexOf(over.id.toString());
+
+        const resultCardsData = arrayMove(activeList.cards, oldCardIndex, newCardIndex);
+
+        setListOrdered((prev) => {
+          const cloneLists = cloneDeep(prev);
+          const targetList = cloneLists.find((c) => c._id === overList._id);
+          if (targetList) {
+            targetList.cards = resultCardsData;
+            targetList.cardOrderIds = targetList.cards.map((c) => c._id);
+          }
+
+          return cloneLists;
+        });
+      }
+    } else {
+      if (active.id !== over.id) {
+        const oldListIndex = listOrderIds.indexOf(active.id.toString());
+        const newListIndex = listOrderIds.indexOf(over.id.toString());
+
+        const resultListData = arrayMove(listOrdered, oldListIndex, newListIndex);
+
+        // * Call API drag end
+        // const resultlistOrderIds = arrayMove(listOrderIds, oldIndex, newIndex);
+        setListOrdered(resultListData);
+      }
     }
 
-    if (active.id !== over.id) {
-      const oldIndex = listOrderIds.indexOf(active.id.toString());
-      const newIndex = listOrderIds.indexOf(over.id.toString());
-
-      const resultListData = arrayMove(listOrdered, oldIndex, newIndex);
-
-      // * Call API drag end
-      // const resultlistOrderIds = arrayMove(listOrderIds, oldIndex, newIndex);
-      setListOrdered(resultListData);
-
-      // Reset Active Data
-      setActiveId(null);
-      setActiveItem(null);
-      return;
-    }
+    // Reset Active Data
+    setActiveListId(null);
+    setActiveItem(null);
   };
 
   return (
@@ -177,7 +210,7 @@ function BoardContent({ data: { listData, listOrderIds } }: Props) {
       </BoardContentWrapper>
       {createPortal(
         <DragOverlay dropAnimation={dropAnimation}>
-          {activeId ? activeItem.listId ? <Card data={activeItem} /> : <Column data={activeItem} /> : null}
+          {activeItem ? activeItem.listId ? <Card data={activeItem} /> : <Column data={activeItem} /> : null}
         </DragOverlay>,
         document.body
       )}
